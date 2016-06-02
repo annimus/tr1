c02e1b7005acfa632628328a5081a6c0fd3f31bd
Eduardo <eduardocalandrini@hotmail.com>
2016-06-02 19:29:01 -0300

    Criando Base do programa Coloquei o while pra fazer testes

--- t1.c
+++ t1.c
@@ -1,83 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define TAM_PKT = x;
+#define TAM_PKT = 100;
 
 int SenderState = 0;
 int ReceiverState = 0;
 
 int main() {
 	int distancia;
-	int tamArq;
+	int tamArq = 200;
 	int numPKTs;
 	int taxa;
+	int i = 0;
+	bool pktLost;
+	srand(unsigned time(null))
 
 	numPKTs = tamArq/TAM_PKT;
 	
+	while(i<tamArq){
+
+		pktLost = senderSend();
+		if (!pktLost)
+		{
+			i++;
+		}
+
+	}
 
 	return 0;
 }
 
 
 //Receiver functions
-void receiverSend(int ack) {
-	senderReceive(ack);
+bool receiverSend(int ack) {
+	return senderReceive(ack);
 }
 
-void receiverReceive(int pkt) {
+bool receiverReceive(int pkt) {
 	switch(ReceiveState) {
 		case 0:
 			if(pkt == 0) {
-				receiverSend(0);
 				ReceiverState = 1;
 				printf("Enviado ack 0\n");
+				return receiverSend(0);
 			} else {
-				receiverSend(1);
 				printf("Enviado ack 1\n");
+				return receiverSend(1);
 			}
-			break;
 		case 1:
 			if(pkt == 1) {
-				receiverSend(1);
 				ReceiverState = 0;
 				printf("Enviado ack 1\n");
+				return receiverSend(1);
 			} else {
-				receiverSend(0);
 				printf("Enviado ack 0\n");
+				return receiverSend(0);
 			}
-			break;
 	}
 }
 
 //Sender functions
-void senderSend(int pkt) {
+bool senderSend(int pkt) {
 	switch(SenderState) {
 		case 0:
-			receiverReceive(pkt);
+			
 			printf("Enviado pkt 0\n");
 			SenderState = 1;
-			break;
+			return receiverReceive(pkt);
 
 		case 2:
-			receiverReceive(pkt);
+			
 			printf("Enviado pkt 1\n");
 			SenderState = 3;
-			break;
+			return receiverReceive(pkt);
 	}	
 }
 
-void senderReceive(int ack) {
+bool senderReceive(int ack) {
 	switch(SenderState) {
 		case 1:
 			if(ack == 0) {
 				SenderState = 2;
 				printf("Recebeu ack 0\n");
+				return false;
 			}
-			break;
+			printf("PKT perdido\n");
+			return true;
 		case 3:
 			if(ack ==1) {
 				SenderState = 0;
 				printf("Recebeu ack 1\n");
+				return false;
 			}
-			break;	
+			printf("PKT perdido\n");
+			return true;
 	}
 }
