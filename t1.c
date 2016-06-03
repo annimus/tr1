08001cae316a7f050d8e9c4525ff51fade32c667
Eduardo Calandrini <eduardocalandrini@hotmail.com>
2016-06-02 21:44:36 -0300

    Arrumando Identação Só oragnizei a identação que estava um pouco exagerada :)

--- t1.c
+++ t1.c
@@ -23,13 +23,13 @@ int main() {
 	_Bool pktLost;
 	srand(time(NULL));
 
-	numPKTs = tamArq/TAM_PKT;
+	numPKTs = tamArq / TAM_PKT
+	;
 
-	while(i<numPKTs){
+	while (i < numPKTs) {
 
 		pktLost = senderSend();
-		if (!pktLost)
-		{
+		if (!pktLost) {
 			i++;
 		}
 
@@ -38,73 +38,72 @@ int main() {
 	return 0;
 }
 
-
 //Receiver functions
 _Bool receiverSend(int ack) {
 	return senderReceive(ack);
 }
 
 _Bool receiverReceive(int pkt) {
-	switch(ReceiverState) {
-		case 0:
-			if(pkt == 0) {
-				ReceiverState = 1;
-				printf("Enviado ack 0\n");
-				return receiverSend(0);
-			} else {
-				printf("Enviado ack 1\n");
-				return receiverSend(1);
-			}
-		case 1:
-			if(pkt == 1) {
-				ReceiverState = 0;
-				printf("Enviado ack 1\n");
-				return receiverSend(1);
-			} else {
-				printf("Enviado ack 0\n");
-				return receiverSend(0);
-			}
+	switch (ReceiverState) {
+	case 0:
+		if (pkt == 0) {
+			ReceiverState = 1;
+			printf("Enviado ack 0\n");
+			return receiverSend(0);
+		} else {
+			printf("Enviado ack 1\n");
+			return receiverSend(1);
+		}
+	case 1:
+		if (pkt == 1) {
+			ReceiverState = 0;
+			printf("Enviado ack 1\n");
+			return receiverSend(1);
+		} else {
+			printf("Enviado ack 0\n");
+			return receiverSend(0);
+		}
 	}
 	return false;
 }
 
 //Sender functions
 _Bool senderSend() {
-	switch(SenderState) {
-		case 0:
+	switch (SenderState) {
+	case 0:
 
-			printf("Enviado pkt 0\n");
-			SenderState = 1;
-			return receiverReceive(0);
+		printf("Enviado pkt 0\n");
+		SenderState = 1;
+		return receiverReceive(0);
 
-		case 2:
+	case 2:
 
-			printf("Enviado pkt 1\n");
-			SenderState = 3;
-			return receiverReceive(1);
+		printf("Enviado pkt 1\n");
+		SenderState = 3;
+		return receiverReceive(1);
 	}
 	return false;
 }
 
 _Bool senderReceive(int ack) {
-	switch(SenderState) {
-		case 1:
-			if(ack == 0) {
-				SenderState = 2;
-				printf("Recebeu ack 0\n");
-				return false;
+	switch (SenderState) {
+	case 1:
+		if (ack == 0) {
+			SenderState = 2;
+			printf("Recebeu ack 0\n");
+			return false;
 
-			}
-			printf("PKT perdido\n");
-			return true;
-		case 3:
-			if(ack ==1) {
-				SenderState = 0;
-				printf("Recebeu ack 1\n");
-				return false;
-			}
-			printf("PKT perdido\n");
-			return true;
+		}
+		printf("PKT perdido\n");
+		return true;
+	case 3:
+		if (ack == 1) {
+			SenderState = 0;
+			printf("Recebeu ack 1\n");
+			return false;
+		}
+		printf("PKT perdido\n");
+		return true;
 	}
 	return false;
 }
