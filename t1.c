e4870482c20e13a423c25159fe09fa825e1f2c21
Eduardo Calandrini <eduardocalandrini@hotmail.com>
2016-06-09 00:56:26 -0300

    Arrumei alguns erros e a apresentação Arrumei o calculo do tempo total que estava errado. E deixei um pouco mais apresentavel. Foi de uma forma nao muto inteligente e nem elegante, mas pelo menos ta melhor...

--- t1.c
+++ t1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #define TAM_PKT 8000;
 #define false 0;
@@ -9,7 +10,8 @@
 unsigned int SenderState = 0;
 unsigned int ReceiverState = 0;
 unsigned int TIMEunit = 0;
-unsigned int timeout = 0;
+double timeout = 0;
+double time_time = 0;
 
 _Bool receiverSend(int ack);
 _Bool receiverReceive(int pkt);
@@ -17,10 +19,17 @@ _Bool senderSend();
 _Bool senderReceive(int ack);
 _Bool senderTimeoutResend(int pkt);
 
+void sendPktPrint();
+void sendAckPrint();
+void sendPkttimeoutPrint();
+void sendAckTimeoutPrint();
+void TimeoutPrint();
+
 int main() {
 
 	int option;
 	int d, x;
+	int tamPKT = TAM_PKT;
 	double distancia = 5000;/*Distancia em metros (5 Km)*/
 	double tamArq = 8000000;/*Tamanho do arquivo em bits (1 MB)*/
 	double taxa = 10000000;/*Taxa de transmissão em bits/s (10 Mb)*/
@@ -29,6 +38,8 @@ int main() {
 	unsigned int numPktLost = 0;
 	_Bool pktLost;
 	double tempototal = 0;
+	/*time_time = 1;*/
+	time_time = (tamPKT/taxa)+(distancia/300000000);
 	srand(time(NULL));
 
 	numPKTs = tamArq / TAM_PKT;
@@ -59,7 +70,7 @@ int main() {
 
 				}
 
-				tempototal = TIMEunit*((tamArq/taxa)+(distancia/300000000));
+				tempototal = TIMEunit*((tamPKT/taxa)+(distancia/300000000));
 
 				printf("\n\nTempo total decorrido: %f segundos\n",tempototal);
 				printf("Numero de pacotes: %d\n",numPKTs);
@@ -78,6 +89,7 @@ int main() {
 				distancia = d;
 				tamArq = x;
 				numPKTs = tamArq / TAM_PKT;
+				time_time = (tamPKT/taxa)+(distancia/300000000);
 
 				while (i < numPKTs) {
 
@@ -90,7 +102,7 @@ int main() {
 
 				}
 
-				tempototal = TIMEunit*((tamArq/taxa)+(distancia/300000000));
+				tempototal = TIMEunit*((tamPKT/taxa)+(distancia/300000000));
 
 				printf("\n\nTempo total decorrido: %f segundos\n",tempototal);
 				printf("Numero de pacotes: %d\n",numPKTs);
@@ -121,10 +133,13 @@ int main() {
 _Bool receiverSend(int ack) {
 	int lose = 1+(rand()%20);
 	if(lose < 20){
+		sendAckPrint();
 		return senderReceive(ack);
 	}else{
 		timeout++;
 		TIMEunit++;
+		sendAckTimeoutPrint();
+		TimeoutPrint();
 		printf("ACK perdido\n");
 		if(ack == 0){
 			SenderState = 0;
@@ -175,18 +190,22 @@ _Bool senderSend() {
 		case 0:
 			timeout = 0;
 			printf("Enviado pkt 0\n");
+			sendPktPrint();
 			SenderState = 1;
 			return receiverReceive(0);
 
 		case 2:
 			timeout = 0;
 			printf("Enviado pkt 1\n");
+			sendPktPrint();
 			SenderState = 3;
 			return receiverReceive(1);
 		}
 	}else{
 		TIMEunit+=2;
 		timeout+=2;
+		sendPkttimeoutPrint();
+		TimeoutPrint();
 		printf("Pacote perdido\n");
 	}
 	return true;
@@ -231,3 +250,79 @@ _Bool senderReceive(int ack) {
 	return true;
 }
 
+/*Apresentacao*/
+void sendPktPrint(){
+	printf("    SENDER   \t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDERPKT\t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\tPKT\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\tPKT\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\tPKT\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\tPKT\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\tPKT\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\t     PKTRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\t\tRECEIVER\tOK\n");
+	sleep(time_time/9);
+}
+
+void sendAckPrint(){
+	printf("    SENDER   \t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\t     ACKRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\tACK\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\tACK\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\tACK\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\tACK\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\tACK\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDERACK\t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("OK  SENDER   \t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+}
+
+void sendPkttimeoutPrint(){
+	printf("    SENDER   \t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDERPKT\t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\tPKT\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\tPKT\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t X \t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t   \t\tRECEIVER\t\n");
+	sleep((13*time_time)/9);
+}
+
+void sendAckTimeoutPrint(){
+	printf("    SENDER   \t\t\t\t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\t     ACKRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\t\tACK\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t\tACK\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t\t X \t\t\tRECEIVER\t\n");
+	sleep(time_time/9);
+	printf("    SENDER\t\t   \t\t\t\tRECEIVER\t\n");
+	sleep((4*time_time)/9);
+}
+
+void TimeoutPrint(){
+	printf("\t\t\tTIMEOUT\t\t\t              \t\n");
+}
